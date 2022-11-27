7dd73eb83faf0f6dd345143fddc19d1e2204bf94
Vijay <vijay.k.vepakomma@gmail.com>
2022-11-27 07:35:56 -0500

    test custom sink

--- AkkaGrpc/akka-grpc-quickstart-java/README.md
+++ AkkaGrpc/akka-grpc-quickstart-java/README.md
@@ -21,6 +21,13 @@ mvn install
 
 ## Documentation
 
-- Guide: https://developer.lightbend.com/guides/akka-grpc-quickstart-java/
-- Streaming Tests: https://doc.akka.io/docs/akka/current/stream/stream-testkit.html?_ga=2.158217082.1998775438.1669423859-273880469.1669423859
-- Local Copy at [Akka grpc Quickstart with Java](docs/Akka%20gRPC%20Quickstart%20with%20Java%20·%20Lightbend%20Tech%20Hub.pdf)
+- [Akka grpc Quickstart Guide](https://developer.lightbend.com/guides/akka-grpc-quickstart-java/)
+- [Streaming Tests](https://doc.akka.io/docs/akka/current/stream/stream-testkit.html?_ga=2.158217082.1998775438.1669423859-273880469.1669423859)
+
+## Local Copy 
+Prints taken in November 2022
+
+- [Akka grpc Quickstart with Java](docs/Akka-gRPC-Quickstart-with-Java.pdf)
+- [Streaming grpc](docs/Streaming-gRPC.pdf)
+- [Testing grpc](docs/Testing-gRPC.pdf)
+- [Testing Streams](docs/Testing-Streams.pdf)
---
+++ AkkaGrpc/akka-grpc-quickstart-java/src/test/java/com/example/helloworld/SampleStreamingTest.java
@@ -0,0 +1,51 @@
+package com.example.helloworld;
+
+import akka.actor.testkit.typed.javadsl.TestKitJunitResource;
+import akka.actor.typed.ActorSystem;
+import akka.stream.javadsl.Flow;
+import akka.stream.javadsl.Keep;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import org.junit.ClassRule;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import static org.junit.Assert.assertEquals;
+
+public class SampleStreamingTest {
+
+
+    @ClassRule
+    public static final TestKitJunitResource TEST_KIT = new TestKitJunitResource();
+    private static final ActorSystem<?> ACTOR_SYSTEM = TEST_KIT.system();
+
+    /**
+     * Testing a custom sink can be as simple as
+     * <li> attaching a source that emits elements from a predefined collection </li>
+     * <li> running a constructed test flow and</li>
+     * <li>asserting on the results that sink produced.</li>
+     *
+     * @throws ExecutionException   execution failed
+     * @throws InterruptedException interrupted
+     * @throws TimeoutException     timed out
+     */
+    @Test
+    public void testCustomSink() throws ExecutionException, InterruptedException, TimeoutException {
+        final Sink<Integer, CompletionStage<Integer>> sinkUnderTest =
+                Flow.of(Integer.class)
+                        .map(i -> i * 2)
+                        .toMat(Sink.fold(0, Integer::sum), Keep.right());
+
+
+        final CompletionStage<Integer> future =
+                Source.from(Arrays.asList(1, 2, 3, 4)).runWith(sinkUnderTest, ACTOR_SYSTEM);
+
+        final Integer result = future.toCompletableFuture().get(3, TimeUnit.SECONDS);
+        assertEquals(20, result.intValue());
+    }
+}
